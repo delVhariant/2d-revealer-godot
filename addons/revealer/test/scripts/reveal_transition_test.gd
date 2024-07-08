9dc17020c1635d0f31ad070755f44689e9c8d31d
delVhar <delvmail+git@pm.me>
2024-07-08 22:11:57 +1000

    test: Begin setting up tests for revealables

---
+++ addons/revealer/test/scripts/reveal_transition_test.gd
@@ -0,0 +1,13 @@
+# GdUnit generated TestSuite
+class_name RevealTransitionTest
+extends GdUnitTestSuite
+@warning_ignore("unused_parameter")
+@warning_ignore("return_value_discarded")
+
+# TestSuite generated from
+const __source = "res://addons/revealer/scripts/reveal_transition.gd"
+
+
+func test_do_reveal() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
--- addons/revealer/test/scripts/revealable_test.gd
+++ addons/revealer/test/scripts/revealable_test.gd
@@ -9,13 +9,9 @@ const __source = "res://addons/revealer/scripts/revealable.gd"
 
 
 func get_dummy(
-	target: Sprite2D = mock(Sprite2D),
-	material: ShaderMaterial = mock(ShaderMaterial),
-	depth: int = 1,
-	parent: Sprite2D = mock(Sprite2D),
-	mask: Sprite2D = mock(Sprite2D)
+	target: Sprite2D = mock(Sprite2D), material: ShaderMaterial = mock(ShaderMaterial)
 ) -> Revealable:
-	return Revealable.new(target, material, depth, parent, mask)
+	return Revealable.new(target, material)
 
 
 func test_reveal() -> void:
@@ -23,7 +19,5 @@ func test_reveal() -> void:
 	var mock_mat = auto_free(mock(ShaderMaterial))
 	mock_sprite.set_material(mock_mat)
 	var mock_revealable = auto_free(get_dummy(mock_sprite))
-	var spy = spy(mock_revealable)
-	const VALUE = 0.5
-	mock_revealable.reveal(VALUE)
-	verify(spy.target.material, 1).set_shader_parameter("revealed_amount", VALUE)
+	assert_object(mock_revealable.target).is_not_null()
+	assert_object(mock_revealable.target.material).is_not_null()
