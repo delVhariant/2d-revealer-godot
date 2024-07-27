0e987fcb47ed213f0b9c40553e0eb432f0cd0108
delVhar <delvmail+git@pm.me>
2024-07-27 14:57:49 +1000

    fix: Remove autoloader as it doesn't work with a backbuffer

--- addons/revealer/plugin.gd
+++ addons/revealer/plugin.gd
@@ -1,14 +1,2 @@
 @tool
 extends EditorPlugin
-
-# Autload the PostProcessLayer
-const PP_AUTOLOAD = "PostProcessLayer"
-
-
-func _enter_tree():
-	# The autoload can be a scene or script file.
-	add_autoload_singleton(PP_AUTOLOAD, "res://addons/revealer/scenes/post_process_layer.tscn")
-
-
-func _exit_tree():
-	remove_autoload_singleton(PP_AUTOLOAD)
