ce9e83804984755fdf72642c928d4aa8879ed2c6
delVhar <delvmail+git@pm.me>
2024-07-27 14:57:24 +1000

    feat: use lambda functions for signals instead of creating new objects

--- addons/revealer/scripts/reveal_layer.gd
+++ addons/revealer/scripts/reveal_layer.gd
@@ -1,31 +1,37 @@
 class_name RevealLayer extends Node2D
 
 signal alpha_changed(value: float)
-signal layer_changed(active: bool, layer)
+signal layer_changed(layer: int)
 signal processing_changed(active: bool)
 
 enum LayerStates { REVEALING, CONCEALING, WAITING }
 
 @export var targets: Array[CanvasItem]
-@export var processing: bool = false
+@export var start_processing: bool = false
 @export_range(0.0, 1.0) var alpha: float = 1.0
 @export_range(0, 10, 0.05) var transition_time: float = 0.4
-@export var state: LayerStates = LayerStates.WAITING
 @export var reveal_states: RevealResource = RevealResource.new()
+@export var no_blur: bool = false
+@export var inverse_blur_state: bool = false
 
-@export var inverse_mask_state: bool = false
-@export var mask_level: int = 0
-@export var mask_sprite: Sprite2D
-
-var reveal_objects: Array[RevealObject] = []
+var state: LayerStates = LayerStates.WAITING
 
 
 func _ready() -> void:
+	# Use lambda functions to map properties to signals
 	for o in targets:
-		RevealObject.new(o, self)
+		alpha_changed.connect(func(value: float): o.modulate.a = value)
+		layer_changed.connect(func(layer: int): o.z_index = layer)
+		processing_changed.connect(
+			func(active: bool): o.process_mode = (
+				self.process_mode if active else Node.PROCESS_MODE_DISABLED
+			)
+		)
+
+	# Emit all signals to initialise everything
 	alpha_changed.emit(alpha)
 	layer_changed.emit(reveal_states.default)
-	processing_changed.emit(processing)
+	processing_changed.emit(start_processing)
 
 
 func _process(delta: float) -> void:
@@ -56,13 +62,12 @@ func start_transition(reveal: bool):
 		self.state = LayerStates.CONCEALING
 		processing_changed.emit(false)
 
-	_register_postprocess(reveal)
+	if !no_blur:
+		_handle_blur(reveal)
 
 
-func _register_postprocess(reveal: bool):
-	if not mask_sprite:
-		return
-
+func _handle_blur(reveal: bool):
+	# Groups are used to support
 	if reveal:
 		if not inverse_blur_state:
 			get_tree().call_group("blur", "enter_layer", self)
